b6cbdf09d5dbe3c091ce79fb5507d4d90de6e0c2
zerven11 <126477050+zerven11@users.noreply.github.com>
2023-06-05 22:04:15 +0700

    Update auth.route.js

--- src pasien/routes/auth.route.js
+++ src pasien/routes/auth.route.js
@@ -1,1 +1,33 @@
+const router = require('express').Router();
+const authController = require('../controllers/auth.controller')
 
+router.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, Content-Type, Accept"
+  )
+
+  next()
+})
+
+router.route('/test')
+  .get((req, res) => {
+    res.status(200)
+      .json({
+        status: 'success',
+        message: 'Test /api/auth/test route success',
+        data: {
+          username: req.body.username,
+          password: req.body.password
+        }
+      })
+  })
+
+router.route('/signup')
+  .post(authController.signup) // TODO: add signup validation
+
+router.route('/signin')
+  .post(authController.signin) // TODO: add signin validation
+
+
+module.exports = router;
