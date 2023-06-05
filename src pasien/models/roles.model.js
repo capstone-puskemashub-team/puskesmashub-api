a702336a773be0ccfdb1d81f58803c692385528c
zerven11 <126477050+zerven11@users.noreply.github.com>
2023-06-05 22:58:55 +0700

    Update roles.model.js

--- src pasien/models/roles.model.js
+++ src pasien/models/roles.model.js
@@ -1,1 +1,15 @@
+const RolesModel = (sequelize, Sequelize) => {
+  const Role = sequelize.define('roles', {
+    roleId: {
+      type: Sequelize.STRING,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+    }
+  })
 
+  return Role
+}
+
+module.exports = RolesModel
