812eef43dc25a6c8bf15279cf05c7a510616ac27
zerven11 <126477050+zerven11@users.noreply.github.com>
2023-06-05 22:59:36 +0700

    Update index.js

--- src pasien/models/index.js
+++ src pasien/models/index.js
@@ -1,1 +1,25 @@
+const { sequelize, Sequelize } = require('../config/db.config')
 
+const db = {}
+
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+db.users = require('./users.model')(sequelize, Sequelize)
+db.roles = require('./roles.model')(sequelize, Sequelize)
+
+db.roles.belongsToMany(db.users, {
+  through: 'user_roles',
+  foreignKey: 'roleId',
+  otherKey: 'userId'
+})
+
+db.users.belongsToMany(db.roles, {
+  through: 'user_roles',
+  foreignKey: 'userId',
+  otherKey: 'roleId'
+})
+
+db.ROLES = ['staff', 'doctor', 'admin']
+
+module.exports = db
