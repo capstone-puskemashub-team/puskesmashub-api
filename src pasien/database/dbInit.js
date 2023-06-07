0cf0590c4213d03d7ebf37729503b10ad29b6e73
zerven11 <126477050+zerven11@users.noreply.github.com>
2023-06-07 21:53:47 +0700

    Update dbInit.js

--- src pasien/database/dbInit.js
+++ src pasien/database/dbInit.js
@@ -1,1 +1,20 @@
+const db = require('../models')
+const { v4: uuid } = require('uuid')
 
+const roleInit = async () => {
+  try {
+    const roles = await db.roles.findAll()
+    if (roles.length === 0) {
+      db.ROLES.forEach(async (role) => {
+        await db.roles.create({
+          roleId: uuid(),
+          name: role
+        })
+      })
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = roleInit
