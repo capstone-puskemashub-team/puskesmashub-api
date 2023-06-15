02cec9582df0809dcb846cdb0051e577408ed411
Ahmad Ali Masykur <ahmad.ali.m@mail.ugm.ac.id>
2023-06-16 00:14:25 +0700

    update env variable and fix db connection config

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,6 +2,16 @@ require('dotenv').config();
 const fs = require('fs')
 const Sequelize = require('sequelize');
 
+let sslConfig = false
+
+if (process.env.NODE_ENV === 'production') {
+  sslConfig = {
+    ca: fs.readFileSync(__dirname + '/../database/cert/server-ca.pem'),
+    key: fs.readFileSync(__dirname + '/../database/cert/client-key.pem'),
+    cert: fs.readFileSync(__dirname + '/../database/cert/client-cert.pem')
+  }
+}
+
 const sequelize = new Sequelize(
   process.env.MYSQL_DB_NAME,
   process.env.MYSQL_DB_USER,
@@ -11,13 +21,9 @@ const sequelize = new Sequelize(
     port: process.env.MYSQL_DB_PORT,
     dialect: 'mysql',
     logging: true,
-    // dialectOptions: {
-    //   ssl: {
-    //     ca: fs.readFileSync(__dirname + '/../database/cert/server-ca.pem'),
-    //     key: fs.readFileSync(__dirname + '/../database/cert/client-key.pem'),
-    //     cert: fs.readFileSync(__dirname + '/../database/cert/client-cert.pem')
-    //   }
-    // },
+    dialectOptions: {
+      ssl: sslConfig
+    },
     pool: {
       max: 5,
       min: 0,
