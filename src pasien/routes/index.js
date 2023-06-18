f795c636f1e4a83be1d4e36d8d7d2f3b1274f670
Ahmad Ali Masykur <ahmad.ali.m@mail.ugm.ac.id>
2023-06-18 20:32:21 +0700

    fix bug in patient controller

--- src pasien/routes/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app')
-
-const PORT = process.env.PORT || 3000
-
-const server = app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
-
-server.timeout = 1000
